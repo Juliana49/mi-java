c68e97adf387951b1eef4002dcc012bbf4ffc794
juliana figueroa <juliana.figueroa557@std.uni.edu.ni>
2025-06-21 11:16:56 -0600

    primer cambio

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args)  {
+        System.out.println("Hello, World!");
+        String nombre = "Esstudiante";
+        int a = 5;
+        int b = 3;
+        int suma = a + b;
+
+        System.out.println("hola"+nombre+"!");
+        System.out.println("la suma de"+a+"+"+b+"es:"+suma);
+    }
+}
